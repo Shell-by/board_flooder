6060b892af87f85a6cfda0554ac304839f1ac6fd
Shell-by <63432079+Shell-by@users.noreply.github.com>
2022-01-06 14:45:10 +0900

    Add files via upload

---
+++ flooder.rb
@@ -0,0 +1,24 @@
+require 'httparty'
+require 'json'
+
+$headers = {
+  "user_id" => "application/json",
+  "User-Agent" => "I'm_gay_Yas"
+}
+
+$data = {
+  "user_id": "I'm gay",
+  "title": "Sorry, I'm actually gay",
+  "content": "https://www.youtube.com/watch?v=P-_GWUw8LwM"
+}
+
+def respone
+  HTTParty.post("http://localhost:8000/board", {
+    :body => $data,
+    :headers => $headers
+  })
+end
+
+while true
+  respone
+end
